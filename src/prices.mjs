2f22766047452344942e649872f74042971a6af6
tobiwinkler <tobias.winkler@helsinki.fi>
2026-02-19 14:15:24 +0200

    tcr: tests pass

--- src/prices.mjs
+++ src/prices.mjs
@@ -84,9 +84,8 @@ function createApp(database) {
     const holidays = database.getHolidays();
     for (let row of holidays) {
       let holiday = Temporal.PlainDate.from(row.holiday);
-      if (date && date.year === holiday.year && date.month === holiday.month && date.day === holiday.day) { return true; }
+      if (date && date.equals(holiday)) { return true; }
     }
-    return false;
   }
 
   return app;
