b2ad75d5f554c590874d7e59c3ff3dc34b75e70b
tobiwinkler <tobias.winkler@helsinki.fi>
2026-02-19 14:08:31 +0200

    tcr: tests pass

--- src/prices.mjs
+++ src/prices.mjs
@@ -71,7 +71,7 @@ function createApp(database) {
 
   function calculateReduction(date, date2) {
     let reduction = 0;
-    if (date && isMonday(date2) && !isHoliday(date, date2)) {
+    if (date && isMonday(date2) && !isHoliday(date2)) {
       reduction = 35;
     }
     return reduction;
@@ -81,7 +81,7 @@ function createApp(database) {
     return date2.dayOfWeek === 1;
   }
 
-  function isHoliday(date, date2) {
+  function isHoliday(date2) {
     const holidays = database.getHolidays();
     for (let row of holidays) {
       let holiday2 = Temporal.PlainDate.from(row.holiday);
