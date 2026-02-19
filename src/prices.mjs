958f27f67f923a44a7b91c9c91a9737ab4623a33
tobiwinkler <tobias.winkler@helsinki.fi>
2026-02-19 14:10:09 +0200

    tcr: tests pass

--- src/prices.mjs
+++ src/prices.mjs
@@ -19,8 +19,8 @@ function createApp(database) {
     const age = req.query.age ? parseInt(req.query.age) : undefined;
     const type = req.query.type;
     const baseCost = database.findBasePriceByType(type).cost;
-    const date2 = parseDate(req.query.date);
-    const cost = calculateCost(age, type, baseCost, date2);
+    const date = parseDate(req.query.date);
+    const cost = calculateCost(age, type, baseCost, date);
     res.json({ cost });
   });
 
